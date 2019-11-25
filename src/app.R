a3dcaf47f090f4c76b84eb83fc77aadc95ef41bc
JeranSalcedo <janraymondsalcedo@gmail.com>
2019-11-26 07:34:52 +0800

    Working Polynomial Regression

--- src/app.R
+++ src/app.R
@@ -29,11 +29,11 @@ ui <- navbarPage(
                         label = h3("Upload CSV"),
                         accept = c("text/csv")
                     ),
-                    numericInput("prNumInput", label = h3("x"), value = 1)
+                    numericInput("prNumInput", label = h3("Degree"), value = 1)
                 ),
                 mainPanel(
                     tableOutput("prTable"),
-                    tableOutput("prSolution")
+                    textOutput("prFunction")
                 )
             )
         )
@@ -83,19 +83,54 @@ server <- function(input, output){
         prData()
     })
     
-    output$prSolution <- renderTable({
+    output$prFunction <- renderPrint({
         if(is.null(prData())){
             return(NULL)
         }
         
-        dataMatrix = matrix(1, nrow(prData()) - 1, nrow(prData()) - 1)
+        l = input$prNumInput + 1
+        dataMatrix = matrix(0, l, l + 1)
+        prFunction = "y ="
         
-        for(y in 1:nrow(prData())){
-            for(x in 1:nrow(prData())){
-                
+        for(y in 1:l){
+            for(x in 1:l){
+                for(i in 1:(nrow(prData()))){
+                    dataMatrix[l + 1 - y, x] = dataMatrix[l + 1 - y, x] + (prData()[i, "x"]) ^ (x + y - 2)
+                }
+            }
+            for(i in 1:(nrow(prData()))){
+                dataMatrix[l + 1 - y, l + 1] = dataMatrix[l + 1 - y, l + 1] + (prData()[i, "y"]) * (prData()[i, "x"]) ^ (y - 1)
             }
         }
-    }, colnames = FALSE)
+        
+        for(i in 1:l){
+            pivotElement = dataMatrix[i, i]
+            for(x in i:(l + 1)){
+                dataMatrix[i, x] = dataMatrix[i, x] / pivotElement
+            }
+            for(y in 1:l){
+                if(y == i){
+                    next
+                }
+                mul = dataMatrix[y, i]
+                for(x in i:(l + 1)){
+                    dataMatrix[y, x] = dataMatrix[y, x] - mul * dataMatrix[i, x]
+                }
+            }
+        }
+        
+        for(i in l:1){
+            prFunction = paste(prFunction, dataMatrix[i, l + 1])
+            if(i == 2){
+                prFunction = paste(prFunction, "x + ", paste = "")
+            } else if(i > 1){
+                prFunction = paste(prFunction, "x ^ ", i - 1, paste = "")
+                prFunction = paste(prFunction, "+")
+            }
+        }
+        
+        prFunction
+    })
 }
 
 shinyApp(ui = ui, server = server)
