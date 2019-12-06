95b067c0fb923eb1dfe9eaf674ed2acdcc17d953
JeranSalcedo <janraymondsalcedo@gmail.com>
2019-12-06 20:35:07 +0800

    Added Objective Function and (supply/demand) Constraints

--- src/app.R
+++ src/app.R
@@ -43,7 +43,7 @@ ui <- navbarPage(
     ),
     tabPanel(
         "Simplex Method",
-        tags$style("#smP1, #smP2, #smP3, #smW1, #smW2, #smW3, #smW4, #smW5, #smS1, #smS2, #smS3 { width: 200px; display: inline-block; }"),
+        tags$style("##smP1, #smP2, #smP3, #smW1, #smW2, #smW3, #smW4, #smW5, #smS1, #smS2, #smS3 { width: 200px; display: inline-block; }"),
         sidebarLayout(
             sidebarPanel(
                 tags$h4("Shipping Cost"),
@@ -54,7 +54,8 @@ ui <- navbarPage(
                 rHandsontableOutput("smTable3")
             ),
             mainPanel(
-                tableOutput("temp")
+                textOutput("smObjF"),
+                verbatimTextOutput("smConstraints")
             )
         )
     ),
@@ -157,42 +158,94 @@ server <- function(input, output){
         prF(input$prNumInput)
     })
     
-    tableData1 = data.frame(w1 = c(5, 6, 3), w2 = c(6, 7, 5), w3 = c(7, 8, 7), w4 = c(8, 9, 11), w5 = c(9, 10, 13))
-    rownames(tableData1) <- c("p1", "p2", "p3")
-    tableData2 = data.frame(w1 = c(100), w2 = c(100), w3 = c(100), w4 = c(100), w5 = c(100))
-    rownames(tableData2) <- c("")
-    tableData3 = data.frame(p1 = c(200), p2 = c(200), p3 = c(200))
-    rownames(tableData3) <- c("")
+    
+    
+    
+    
+    tableData1 = reactiveVal({
+        table1 = data.frame(w1 = c(5, 6, 3), w2 = c(6, 7, 5), w3 = c(7, 8, 7), w4 = c(8, 9, 11), w5 = c(9, 10, 13))
+        rownames(table1) <- c("p1", "p2", "p3")
+        
+        table1
+    })
+    tableData2 = reactiveVal({
+        table2 = data.frame(w1 = c(100), w2 = c(100), w3 = c(100), w4 = c(100), w5 = c(100))
+        rownames(table2) <- c("")
+        
+        table2
+    })
+    tableData3 = reactiveVal({
+        table3 = data.frame(p1 = c(200), p2 = c(200), p3 = c(200))
+        rownames(table3) <- c("")
+        
+        table3
+    })
     
     # referenced from https://stackoverflow.com/questions/22272571/data-input-via-shinytable-in-r-shiny-application
     observe({
         if(!is.null(input$smTable1)){
-            tableData1 = hot_to_r(input$smTable1)
+            tableData1(hot_to_r(input$smTable1))
         }
         if(!is.null(input$smTable2)){
-            tableData2 = hot_to_r(input$smTable2)
+            tableData2(hot_to_r(input$smTable2))
         }
         if(!is.null(input$smTable3)){
-            tableData3 = hot_to_r(input$smTable3)
+            tableData3(hot_to_r(input$smTable3))
         }
     })
     
     output$smTable1 <- renderRHandsontable({
-        if(!is.null(tableData1)){
-            rhandsontable(tableData1, stretchH = "all")
+        if(!is.null(tableData1())){
+            rhandsontable(tableData1(), stretchH = "all")
         }
     })
     
     output$smTable2 <- renderRHandsontable({
-        if(!is.null(tableData2)){
-            rhandsontable(tableData2, stretchH = "all")
+        if(!is.null(tableData2())){
+            rhandsontable(tableData2(), stretchH = "all")
         }
     })
     
     output$smTable3 <- renderRHandsontable({
-        if(!is.null(tableData3)){
-            rhandsontable(tableData3, stretchH = "all")
+        if(!is.null(tableData3())){
+            rhandsontable(tableData3(), stretchH = "all")
         }
+    })
+    
+    output$smObjF <- renderText({
+        objF = "Min Z = "
+        
+        for(x in 1:ncol(tableData1())){
+            for(y in 1:nrow(tableData1())){
+                objF = paste(objF, tableData1()[y, x], " * m(", rownames(tableData1())[y], ", ", colnames(tableData1())[x], ")", sep = "")
+                if(x < ncol(tableData1()) || y < nrow(tableData1())){
+                    objF = paste(objF, "+ ")
+                }
+            }
+        }
+        
+        objF
+    })
+    
+    output$smConstraints <- renderPrint({
+        constraints = c()
+        
+        for(y in 1:nrow(tableData1())){
+            constraint = ""
+            for(x in 1:ncol(tableData1())){
+                constraint = paste(constraint, "m(", rownames(tableData1())[y], ", ", colnames(tableData1())[x], ")", sep = "")
+                if(x < ncol(tableData1())){
+                    constraint = paste(constraint, "+ ")
+                }
+            }
+            supplyConstraint = paste(constraint, "<=", tableData3()[1, y])
+            demmandConstraint = paste(constraint, ">=", tableData2()[1, y])
+            
+            constraints[(y - 1) * 2 + 1] = supplyConstraint
+            constraints[(y - 1) * 2 + 2] = demmandConstraint
+        }
+        
+        constraints
     })
 }
 
